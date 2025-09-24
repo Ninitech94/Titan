e817f488d126035d91c398a0d25094ff75f4bb8b
Mitar <mitar.bogunovic@vetatek.com>
2025-09-24 14:07:44 +0200

    Initial commit (clean snapshot)

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <div id="app">
+    <Header />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Arial', sans-serif;
+  line-height: 1.6;
+  color: #333;
+}
+
+#app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+main {
+  flex: 1;
+}
+</style>
